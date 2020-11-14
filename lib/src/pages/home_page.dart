1eb3a5932c1d3ffb8067acc713ec7770da9f642e
herwin <hertosan29@gmail.com>
2020-11-13 22:23:41 -0500

    Cambios archivo json

---
+++ lib/src/pages/home_page.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:no_te_enredes_app/src/providers/menu_provider.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Menú Principal'),
+      ),
+      body: _lista(),
+    );
+  }
+
+  Widget _lista() {
+    // menuProvider.cargarData()
+    return FutureBuilder(
+      future: menuProvider.cargarData(),
+      initialData: [],
+      builder: (BuildContext context, AsyncSnapshot<List<dynamic>> snapshot) {
+        return ListView(
+          children: _listItems(snapshot.data),
+        );
+      },
+    );
+  }
+
+  List<Widget> _listItems(List<dynamic> data) {
+    final List<Widget> opciones = [];
+    data.forEach((opt) {
+      final widgetTemp = ListTile(
+        title: Text(opt['texto']),
+        leading: Icon(Icons.account_circle, color: Colors.blue),
+        trailing: Icon(Icons.keyboard_arrow_right, color: Colors.blue),
+        onTap: () {},
+      );
+      opciones..add(widgetTemp)..add(Divider());
+    });
+    return opciones;
+  }
+}
